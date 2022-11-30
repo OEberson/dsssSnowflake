1628b7b1b07aecfa76f79466f28cfafb0754b60c
Ole Eberson <ole.eberson@fau.de>
2022-11-30 16:26:15 +0100

    removed commas again

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 VERSION= '0.1.0'
 
 setup(
-    name='dsssSnowflakeOEberson',
-    version=VERSION,
-    description='DSSS homework - draws random colored snowflakes',
-    url='https://github.com/OEberson/dsssSnowflake',
-    license='Apache License',
-    packages=find_packages(),
+    name='dsssSnowflakeOEberson'
+    version=VERSION
+    description='DSSS homework - draws random colored snowflakes'
+    url='https://github.com/OEberson/dsssSnowflake'
+    license='Apache License'
+    packages=find_packages()
     install_requires=['numpy','turtle']
 )
