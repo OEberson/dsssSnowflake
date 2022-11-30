fb933db04f3dd4c193d519e465fa102d838b5800
Ole Eberson <ole.eberson@fau.de>
2022-11-30 16:19:19 +0100

    changed syntax

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dsssSnowflakeOEberson',
-    #version='0.1.0',
-    description='DSSS homework - draws random colored snowflakes',
-    url='https://github.com/OEberson/dsssSnowflake',
-    license='Apache License',
-    packages=find_packages(),
+    name='dsssSnowflakeOEberson'
+    version='0.1.0'
+    description='DSSS homework - draws random colored snowflakes'
+    url='https://github.com/OEberson/dsssSnowflake'
+    license='Apache License'
+    packages=find_packages()
     install_requires=['numpy','turtle']
 )
