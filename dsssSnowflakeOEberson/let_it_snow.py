93e3af123febf6079b2be3b12765997cdd8989b0
Ole Eberson <ole.eberson@fau.de>
2022-11-30 20:48:38 +0100

    moved main to end of file

--- dsssSnowflakeOEberson/let_it_snow.py
+++ dsssSnowflakeOEberson/let_it_snow.py
@@ -2,6 +2,23 @@ import turtle
 import numpy as np
 
 
+def snowflake_branch(size, myTurtle):
+    # This function draws one branch of the snowflake.
+    for _ in range(3):
+        for _ in range(3):
+            myTurtle.forward(size / 3)
+            myTurtle.backward(size / 3)
+            myTurtle.right(45)
+        myTurtle.left(90)
+        myTurtle.backward(size / 3)
+        myTurtle.left(45)
+    myTurtle.right(90)
+    myTurtle.forward(size)
+
+
+if __name__ == "__main__":
+    main()
+
 def main(speed=0, bg_color="white"):
     # create Turtle object
     turtle_screen = turtle.Screen()
@@ -34,21 +51,3 @@ def main(speed=0, bg_color="white"):
         for _ in range(8):
             snowflake_branch(size, myTurtle)
             myTurtle.left(45)
-
-
-def snowflake_branch(size, myTurtle):
-    # This function draws one branch of the snowflake.
-    for _ in range(3):
-        for _ in range(3):
-            myTurtle.forward(size / 3)
-            myTurtle.backward(size / 3)
-            myTurtle.right(45)
-        myTurtle.left(90)
-        myTurtle.backward(size / 3)
-        myTurtle.left(45)
-    myTurtle.right(90)
-    myTurtle.forward(size)
-
-
-if __name__ == "__main__":
-    main()
