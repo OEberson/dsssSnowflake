56a1052518210bcde62f08481c484e702dc892d5
Ole Eberson <ole.eberson@fau.de>
2022-11-30 16:52:08 +0100

    I hope no one ever reads these

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 #VERSION= '0.1.0'
 
 setup(
-    # name = 'dsssSnowflakeOEberson',
+     name = 'dsssSnowflakeOEberson'
     # #version = VERSION,
     # description = 'DSSS homework - draws random colored snowflakes',
     # url = 'https://github.com/OEberson/dsssSnowflake',
