7bc7197d5be7daf4315be7adce19429534962b18
Ole Eberson <ole.eberson@fau.de>
2022-11-30 16:29:47 +0100

    removed version number

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-VERSION= '0.1.0'
+#VERSION= '0.1.0'
 
 setup(
     name = 'dsssSnowflakeOEberson'
-    version = VERSION
+    #version = VERSION
     description = 'DSSS homework - draws random colored snowflakes'
     url = 'https://github.com/OEberson/dsssSnowflake'
     license = 'Apache License'
