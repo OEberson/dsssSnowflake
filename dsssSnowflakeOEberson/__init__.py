9cd753f5c4d37e7b0789383ab92ad89c40799a10
Ole Eberson <ole.eberson@fau.de>
2022-11-30 14:28:56 +0100

    finished package

---
+++ dsssSnowflakeOEberson/__init__.py
@@ -0,0 +1,2 @@
+from let_it_snow import main
+from let_it_snow import snowflake_branch
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='dsssSnowflake',
+    name='dsssSnowflakeOEberson',
     version='0.1.0',
     description='DSSS homework - draws random colored snowflakes',
     url='https://github.com/OEberson/dsssSnowflake',
     license='Apache License Version 2.0',
-    packages='dsssSnowflake',
+    packages=find_packages(),
     install_requires=['numpy','turtle']
 
 )
