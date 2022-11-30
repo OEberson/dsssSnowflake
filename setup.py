b1d9bc77f43ffa605d0cf8b9485d1e82e9945dd1
Ole Eberson <ole.eberson@fau.de>
2022-11-30 13:54:14 +0100

    added files to create a python package - incompl.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsssSnowflake',
+    version='0.1.0',
+    description='DSSS homework - draws random colored snowflakes',
+    url='https://github.com/OEberson/dsssSnowflake',
+    license='Apache License Version 2.0',
+    packages='dsssSnowflake',
+    install_requires=['numpy','turtle'],
+    
+)
