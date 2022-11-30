623cba5be6582da49a33852662e01aaccec737ac
Ole Eberson <ole.eberson@fau.de>
2022-11-30 16:18:18 +0100

    remove version tag in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsssSnowflakeOEberson',
-    version='0.1.0',
+    #version='0.1.0',
     description='DSSS homework - draws random colored snowflakes',
     url='https://github.com/OEberson/dsssSnowflake',
     license='Apache License',
